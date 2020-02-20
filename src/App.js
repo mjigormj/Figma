22013a1f89c7791f83ba010afe53d9ebfc93c8be
Igor Cruz <47676774+mjigormj@users.noreply.github.com>
2020-02-20 02:07:44 -0300

    Figma padrão 14/02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
+class App extends Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
       lista: [
@@ -19,46 +19,54 @@ class App extends Component{
 
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch("http://localhost:8080/api/projetos")
-    .then(res => res.json())
-    .then((result) =>
-      this.setState({lista: result.data})
-    )
+      .then(res => res.json())
+      .then((result) =>
+        this.setState({ lista: result.data })
+      )
   }
 
-render() {
-  return (
-    <>
-    <div class="ui menu inverted fixed">
-  <a class="item">roberto</a>
-  <button class="ui button primary"> criar projeto</button>
-  <div class="ui icon input">
-  <input type="text" placeholder="Search..." />
-  <i aria-hidden="true" class="search icon"></i>
-</div>
-</div>
+  render() {
+    return (
+      <>
+        <div class="ui menu inverted fixed">
+          <a class="item">roberto</a>
+          <button class="ui button primary"> criar projeto</button>
+          <div class="ui icon input">
+            <input type="text" placeholder="Search..." />
+            <i aria-hidden="true" class="search icon"></i>
+          </div>
+        </div>
 
-<div class="ui grid three column container">
-  <div class="column">
-  <div class="ui card">
-  <div class="image"><img src="/images/avatar/large/matthew.png" /></div>
-  <div class="content">
-  <div class="header">{projeto.nome}</div>
-  <div class="meta"><span class="date">{projeto.usuario}</span></div>
-    <div class="description">Matthew is a musician living in Nashville.</div>
-  </div>
-  <div class="extra content">
-    <a>
-      <i aria-hidden="true" class="user icon"></i>
-      22 Friends
-    </a>
-  </div>
-</div>
-  </div>
-</div>
-</>
-  );
-}
+        <div class="ui grid three column container">
+          {
+            this.state.lista.map(function (projeto) {
+              return(
+                <div class="column" >
+                  <div class="ui card">
+                    <div class="image"><img src="/images/avatar/large/matthew.png" /></div>
+                    <div class="content">
+                      <div class="header">{projeto.nome}</div>
+                      <div class="meta"><span class="date">{projeto.usuario}</span></div>
+                      <div class="description">Matthew is a musician living in Nashville.</div>
+                    </div>
+                    <div class="extra content">
+                      <a>
+                        <i aria-hidden="true" class="user icon"></i>
+                        22 Friends
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                )
+              }             
+              )
+            }
+        </div>
+      </>  
+            
+      );
+    }
 }
 export default App;
